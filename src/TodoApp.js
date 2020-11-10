423a6656edc5ed4f696e05f5343a0e7015a07692
lilshebeary <cirijaye@yahoo.com>
2020-11-10 06:34:12 -0800

    Add TodoForm with function add useInputState hook

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
@@ -14,6 +15,9 @@ function TodoApp() {
     {id: 1, task: "Walk Dog", completed: false}
 ];
   const [todos, setTodos] = useState(initialTodos);
+  const addTodo = newTodoText => {
+    setTodos([...todos, { id: 4, task: newTodoText, completed: false }])
+  }
   return (
     <Paper style={{
       padding: 0,
@@ -32,6 +36,7 @@ function TodoApp() {
           <Typography color='inherit'>TODOS WITH HOOKS</Typography>
         </Toolbar>
       </AppBar>
+      <TodoForm addTodo={addTodo} />
       <TodoList todos={todos} />
 
     </Paper>
